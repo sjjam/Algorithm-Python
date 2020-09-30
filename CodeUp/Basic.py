d4aee69b61edcc134b32cb782e335d935b0c8cd2
sjjam <shimjm21@gmail.com>
2020-09-30 16:50:49 +0900

    09/30 codeup 1~30

---
+++ CodeUp/Basic.py
@@ -0,0 +1,107 @@
+# 1001
+# print('Hello')
+
+# 1002
+# print('Hello World')
+
+# 1003
+# print('Hello\nWorld')
+
+# 1004
+# print("'Hello'")
+
+# 1005
+# print('"Hello World"')
+
+# 1006
+# print('"!@#$%^&*()"')
+
+# 1007
+# print('"C:\Download\hello.cpp"')
+
+# 1008
+# print("\u250C\u252C\u2510\n\u251C\u253C\u2524\n\u2514\u2534\u2518")
+
+# 1010
+# a = input()
+# print(a)
+
+# 1011
+# a = input()
+# print(a)
+
+# 1012
+# a = float(input())
+# print('%f' %a)
+
+# 1013
+# a, b = map(int, input().split())
+# print(a, b)
+
+# 1014
+# a, b = map(str, input().split())
+# print(b, a)
+
+# 1015
+# a = float(input())
+# print('%.2f' % a)
+
+# 1017
+# a = int(input())
+# print(a, a, a)
+
+# 1018
+# a, b = map(int, input().split(":"))
+# print(a, b, sep=":")
+
+# 1019
+# y, m, d = map(int, input().split("."))
+# print('%04d' %y, '%02d' %m, '%02d' %d, sep=".")
+
+# 1020
+# a, b = map(int, input().split("-"))
+# print(a, b, sep='')
+
+# 1021
+# a = str(input())
+# print(a)
+
+# 1022
+# a = str(input())
+# print(a)
+
+# 1023
+# a, b = input().split('.')
+# print(a)
+# print(b)
+
+# 1024
+# a = input()
+# for i in range(len(a)):
+#     print("'" + a[i] + "'")
+
+# 1025
+# a = input()
+# for i in range(len(a)):
+#     b = "0"*(4-i)
+#     print("["+a[i]+b+"]")
+
+# 1026
+# h, m, s = map(int, input().split(":"))
+# print(m)
+
+# 1027
+# y, m, d = map(str, input().split("."))
+# print(d + "-" + m + "-" + y)
+
+# 1028
+# a = input()
+# print(a)
+
+# 1029
+# a = float(input())
+# print('%.11f' % a)
+
+# 1030
+# a = input()
+# print(a)
