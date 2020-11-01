1d87032b6ac0c1c78c133d93f08d75a87c76e2e8
sjjam <shimjm21@gmail.com>
2020-11-01 21:02:26 +0900

    11/01 이진탐색 2번

--- Book/Binary Search/7-2.py
+++ Book/Binary Search/7-2.py
@@ -5,14 +5,43 @@ array = list(map(int, input().split()))
 
 array.sort()
 
-def search(array, target, start, end):
-    while start <= end:
-        mid = (start + end) // 2
-        if array[mid] >= target:
-            return mid
-        else:
-            start = mid + 1
-    return None
+start = 0
+end = array[n - 1]
+while True:
+    mid = (start + end) // 2
+    sum = 0
+    for i in array:
+        if i > mid:
+            sum += i - mid
+    if sum == m:
+        break
+    elif sum > m:
+        start = mid + 1
+    else:
+        end = mid - 1
 
-for i in array:
-    
+print(mid)
+
+
+# 풀이
+
+n, m = list(map(int, input().split(' ')))
+array = list(map(int, input().split()))
+
+start = 0
+end = max(array)
+
+result = 0
+while(start <= end):
+    total = 0
+    mid = (start + end) // 2
+    for x in array:
+        if x > mid:
+            total += x - mid
+    if total < m:
+        end = mid - 1
+    else:
+        result = mid
+        start = mid + 1
+
+print(result)
