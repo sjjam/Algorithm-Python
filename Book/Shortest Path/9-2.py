6dc5f19d3c5a7c623eee6c8f14f652adb4680b93
sjjam <shimjm21@gmail.com>
2020-11-13 21:53:52 +0900

    11/13 최단경로 2번

--- Book/Shortest Path/9-2.py
+++ Book/Shortest Path/9-2.py
@@ -8,8 +8,8 @@ graph = [[] for i in range(n + 1)]
 distance = [INF] * (n + 1)
 
 for _ in range(m):
-    a, b, c = map(int, input().split())
-    graph[a].append((b, c))
+    x, y, z = map(int, input().split())
+    graph[x].append((y, z))
 
 def dijkstra(start):
     q = []
@@ -26,39 +26,55 @@ def dijkstra(start):
                 heapq.heappush(q, (cost, i[0]))
 
 dijkstra(c)
-print(distance)
 
+num = 0
+time = 0
+for i in distance:
+    if i > 0 and i < INF:
+        num += 1
+        if i > time:
+            time = i
+
+print(num, time)
 
 
 # 풀이
 
-# import heapq
-# import sys
+import heapq
+import sys
 
-# input = sys.stdin.readline
-# INF = int(1e9)
+input = sys.stdin.readline
+INF = int(1e9)
 
-# n, m, start = map(int, input().split())
-# graph = [[] for i in range(n + 1)]
-# distance = [INF] * (n + 1)
+n, m, start = map(int, input().split())
+graph = [[] for i in range(n + 1)]
+distance = [INF] * (n + 1)
 
-# for _ in range(m):
-#     x, y, z = map(int, input().split())
-#     graph[x].append((y, z))
+for _ in range(m):
+    x, y, z = map(int, input().split())
+    graph[x].append((y, z))
 
-# def dijkstra(start):
-#     q = []
-#     heapq.heappush(q, (0, start))
-#     distance[start] = 0
-#     while q:
-#         dist, now = heapq.heappop(q)
-#         if distance[now] < dist:
-#             continue
-#         for i in graph[now]:
-#             cost = dist + i[1]
-#             if cost < distance[i[0]]:
-#                 distance[i[0]] = cost
-#                 heapq.heappush(q, (cost, i[0]))
+def dijkstra(start):
+    q = []
+    heapq.heappush(q, (0, start))
+    distance[start] = 0
+    while q:
+        dist, now = heapq.heappop(q)
+        if distance[now] < dist:
+            continue
+        for i in graph[now]:
+            cost = dist + i[1]
+            if cost < distance[i[0]]:
+                distance[i[0]] = cost
+                heapq.heappush(q, (cost, i[0]))
 
-# dijkstra(start)
-# print(distance)
+dijkstra(start)
+
+count = 0
+max_distance = 0
+for d in distance:
+    if d != INF:
+        count += 1
+        max_distance = max(max_distance, d)
+
+print(count - 1, max_distance)
