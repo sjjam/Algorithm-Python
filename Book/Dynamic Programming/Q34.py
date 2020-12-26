800f18f63e6487a853c7f746ebeb31dee4bf1bd9
sjjam <shimjm21@gmail.com>
2020-12-26 14:26:40 +0900

    12/26 DP 기출 34번

---
+++ Book/Dynamic Programming/Q34.py
@@ -0,0 +1,20 @@
+# 병사 배치하기
+# https://www.acmicpc.net/problem/18353
+
+
+
+
+# 풀이
+
+n = int(input())
+array = list(map(int, input().split()))
+array.reverse()
+print(array)
+dp = [1] * n
+
+for i in range(1, n):
+    for j in range(0, i):
+        if array[j] < array[i]:
+            dp[i] = max(dp[i], dp[j] + 1)
+print(dp)
+print(n - max(dp))
