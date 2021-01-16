88245faeb78269871599fee936625c518fb14719
sjjam <shimjm21@gmail.com>
2021-01-16 16:26:01 +0900

    01/16 greedy Q6 review

--- Book/Review/greedy.py
+++ Book/Review/greedy.py
@@ -150,13 +150,42 @@
 # 볼링공 고르기
 # r1
 
-n, m = map(int, input().split())
-data = list(map(int, input().split()))
+# n, m = map(int, input().split())
+# data = list(map(int, input().split()))
 
-ans = 0
-for i in range(len(data) - 1):
-    for j in range(i + 1, len(data)):
-        if data[i] != data[j]:
-            ans += 1
+# ans = 0
+# for i in range(len(data) - 1):
+#     for j in range(i + 1, len(data)):
+#         if data[i] != data[j]:
+#             ans += 1
 
-print(ans)
+# print(ans)
+
+
+
+# 무지의 먹방 라이브
+# r1 x
+# https://programmers.co.kr/learn/courses/30/lessons/42891
+
+import heapq
+
+def solution(food_times, k):
+    if sum(food_times) <= k:
+        return -1
+    
+    q = []
+    for i in range(len(food_times)):
+        heapq.heappush(q, (food_times[i], i + 1))
+    
+    sum_value = 0
+    previous = 0
+    length = len(food_times)
+    
+    while sum_value + ((q[0][0] - previous) * length) <= k:
+        now = heapq.heappop(q)[0]
+        sum_value += (now - previous) * length
+        length -= 1
+        previous = now
+    
+    result = sorted(q, key=lambda x: x[1])
+    return result[(k - sum_value) % length][1]
