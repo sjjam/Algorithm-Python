1e0878a5311cfcfd732bce898e0ba9f232a5c152
sjjam <shimjm21@gmail.com>
2021-01-18 22:36:44 +0900

    01/18 단속카메라

--- programmers/type/Greedy/42884.py
+++ programmers/type/Greedy/42884.py
@@ -21,7 +21,7 @@ def solution(routes):
     answer = 0
     routes.sort(key=lambda x: x[1])
     camera = -30001
-    print(routes)
+
     for route in routes:
         if camera < route[0]:
             answer += 1
