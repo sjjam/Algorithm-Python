365ee927dc46af6848541ea02e930b70be860368
sjjam <shimjm21@gmail.com>
2021-01-25 16:00:25 +0900

    01/25 구현 Q11

--- Book/Review/implementation.py
+++ Book/Review/implementation.py
@@ -201,43 +201,162 @@
 # r1 x
 # https://programmers.co.kr/learn/courses/30/lessons/60059
 
-def rotate_a_matrix_by_90_degree(a):
-    n = len(a)
-    m = len(a[0])
-    result = [[0] * n for _ in range(m)]
-    for i in range(n):
-        for j in range(m):
-            result[j][n - i - 1] = a[i][j]
-    return result
+# def rotate_a_matrix_by_90_degree(a):
+#     n = len(a)
+#     m = len(a[0])
+#     result = [[0] * n for _ in range(m)]
+#     for i in range(n):
+#         for j in range(m):
+#             result[j][n - i - 1] = a[i][j]
+#     return result
 
-def check(new_lock):
-    lock_length = len(new_lock) // 3
-    for i in range(lock_length, lock_length * 2):
-        for j in range(lock_length, lock_length * 2):
-            if new_lock[i][j] != 1:
-                return False
-    return True
+# def check(new_lock):
+#     lock_length = len(new_lock) // 3
+#     for i in range(lock_length, lock_length * 2):
+#         for j in range(lock_length, lock_length * 2):
+#             if new_lock[i][j] != 1:
+#                 return False
+#     return True
 
-def solution(key, lock):
-    n = len(lock)
-    m = len(key)
-    new_lock = [[0] * (n * 3) for _ in range(n * 3)]
+# def solution(key, lock):
+#     n = len(lock)
+#     m = len(key)
+#     new_lock = [[0] * (n * 3) for _ in range(n * 3)]
 
-    for i in range(n):
-        for j in range(n):
-            new_lock[i + n][j + n] = lock[i][j]
+#     for i in range(n):
+#         for j in range(n):
+#             new_lock[i + n][j + n] = lock[i][j]
     
-    for rotation in range(4):
-        key = rotate_a_matrix_by_90_degree(key)
-        for x in range(n * 2):
-            for y in range(n * 2):
-                for i in range(m):
-                    for j in range(m):
-                        new_lock[x + i][y + j] += key[i][j]
+#     for rotation in range(4):
+#         key = rotate_a_matrix_by_90_degree(key)
+#         for x in range(n * 2):
+#             for y in range(n * 2):
+#                 for i in range(m):
+#                     for j in range(m):
+#                         new_lock[x + i][y + j] += key[i][j]
                 
-                if check(new_lock) == True:
-                    return True
-                for i in range(m):
-                    for j in range(m):
-                        new_lock[x + i][y + j] -= key[i][j]
-    return False
+#                 if check(new_lock) == True:
+#                     return True
+#                 for i in range(m):
+#                     for j in range(m):
+#                         new_lock[x + i][y + j] -= key[i][j]
+#     return False
+
+
+
+# 뱀
+# r1 x
+# https://www.acmicpc.net/problem/3190
+
+from collections import deque
+
+n = int(input())
+k = int(input())
+arr = [[0] * n for _ in range(n)]
+for _ in range(k):
+    a, b = map(int, input().split())
+    arr[a - 1][b - 1] = 2
+l = int(input())
+change = []
+for _ in range(l):
+    x, d = input().split()
+    change.append((x, d))
+
+dx = [0, 1, 0, -1]
+dy = [1, 0, -1, 0]
+idx = 0
+snake = deque()
+snake.append([0, 0])
+arr[0][0] = 1
+time = 0
+
+while True:
+    x, y = snake.pop()
+    nx = x + dx[idx]
+    ny = y + dy[idx]
+
+    if nx < 0 or nx >= n or ny < 0 or ny >= n:
+        time += 1
+        break
+    if arr[nx][ny] == 1:
+        time += 1
+        break
+    
+    if arr[nx][ny] == 0:
+        arr[nx][ny] = 1
+        snake.append([x, y])
+        snake.append([nx, ny])
+        px, py = snake.popleft()
+        arr[px][py] = 0
+    if arr[nx][ny] == 2:
+        arr[nx][ny] = 1
+        snake.append([x, y])
+        snake.append([nx, ny])
+
+    time += 1
+    for x, d in change:
+        if int(x) == time:
+            if d == 'D':
+                idx = (idx + 1) % 4
+            elif d == 'L':
+                idx = (idx - 1) % 4
+
+print(time)
+
+n = int(input())
+k = int(input())
+data = [[0] * (n + 1) for _ in range(n + 1)]
+info = []
+
+for _ in range(k):
+    a, b = map(int, input().split())
+    data[a][b] = 1
+
+l = int(input())
+for _ in range(l):
+    x, c = input().split()
+    info.append((int(x), c))
+
+dx = [0, 1, 0, -1]
+dy = [1, 0, -1, 0]
+
+def turn(direction, c):
+    if c == 'L':
+        direction = (direction - 1) % 4
+    else:
+        direction = (direction + 1) % 4
+    return direction
+
+def simulate():
+    x, y = 1, 1
+    data[x][y] = 2
+    direction = 0
+    time = 0
+    index = 0
+    q = [(x, y)]
+    while True:
+        nx = x + dx[direction]
+        ny = y + dy[direction]
+
+        if 1 <= nx and nx <= n and 1 <= ny and ny <= n and data[nx][ny] != 2:
+            if data[nx][ny] == 0:
+                data[nx][ny] = 2
+                q.append((nx, ny))
+                px, py = q.pop(0)
+                data[px][py] = 0
+            
+            if data[nx][ny] == 1:
+                data[nx][ny] = 2
+                q.append((nx, ny))
+        else:
+            time += 1
+            break
+
+        x, y = nx, ny
+        time += 1
+        if index < l and time == info[index][0]:
+            direction = turn(direction, info[index][1])
+            index += 1
+    return time
+
+print(simulate())
