72efaf9196fd559d595b524079feeb81bba5788b
sjjam <shimjm21@gmail.com>
2021-01-29 16:38:46 +0900

    01/29 dfs/bfs Q16 review

--- Book/Review/dfs_bfs.py
+++ Book/Review/dfs_bfs.py
@@ -104,32 +104,96 @@
 # r1 x
 # https://www.acmicpc.net/problem/18352
 
+# from collections import deque
+
+# n, m, k, x = map(int, input().split())
+# graph = [[] for _ in range(n + 1)]
+
+# for _ in range(m):
+#     a, b = map(int, input().split())
+#     graph[a].append(b)
+
+# distance = [-1] * (n + 1)
+# distance[x] = 0
+
+# q = deque([x])
+# while q:
+#     now = q.popleft()
+#     for next_node in graph[now]:
+#         if distance[next_node] == -1:
+#             distance[next_node] = distance[now] + 1
+#             q.append(next_node)
+
+# check = False
+# print(distance)
+# for i in range(1, n + 1):
+#     if distance[i] == k:
+#         print(i)
+#         check = True
+
+# if check == False:
+#     print(-1)
+
+
+
+# 연구소
+# r1
+# https://www.acmicpc.net/problem/14502
+
+from itertools import combinations
 from collections import deque
+import copy
 
-n, m, k, x = map(int, input().split())
-graph = [[] for _ in range(n + 1)]
+n, m = map(int, input().split())
+arr = []
+virus = []
+none = []
 
-for _ in range(m):
-    a, b = map(int, input().split())
-    graph[a].append(b)
+for i in range(n):
+    data = list(map(int, input().split()))
+    arr.append(data)
+    for j in range(len(data)):
+        if data[j] == 2:
+            virus.append((i, j))
+        if data[j] == 0:
+            none.append((i, j))
 
-distance = [-1] * (n + 1)
-distance[x] = 0
+dx = [-1, 0, 1, 0]
+dy = [0, -1, 0, 1]
+safe = 0
 
-q = deque([x])
-while q:
-    now = q.popleft()
-    for next_node in graph[now]:
-        if distance[next_node] == -1:
-            distance[next_node] = distance[now] + 1
-            q.append(next_node)
+def spread(tmp):
+    for i in virus:
+        q = deque()
+        x, y = i
+        q.append((x, y))
 
-check = False
-print(distance)
-for i in range(1, n + 1):
-    if distance[i] == k:
-        print(i)
-        check = True
+        while q:
+            x, y = q.popleft()
+            for j in range(4):
+                nx = x + dx[j]
+                ny = y + dy[j]
 
-if check == False:
-    print(-1)
+                if nx >= 0 and nx < n and ny >=0 and ny < m:
+                    if tmp[nx][ny] == 0:
+                        tmp[nx][ny] = 2
+                        q.append((nx, ny))
+
+ans = 0
+
+for i in list(combinations(none, 3)):
+    tmp = copy.deepcopy(arr)
+    for j in i:
+        tmp[j[0]][j[1]] = 1
+    
+    spread(tmp)
+    cnt = 0
+    
+    for j in range(n):
+        for k in range(m):
+            if tmp[j][k] == 0:
+                cnt += 1
+    
+    ans = max(ans, cnt)
+
+print(ans)
