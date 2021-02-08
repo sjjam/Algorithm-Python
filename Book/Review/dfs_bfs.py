d3c828d87ffdd43a7ed30fea3fe334e82bb077dd
sjjam <shimjm21@gmail.com>
2021-02-08 15:51:21 +0900

    02/08 dfs/bfs Q22 review

--- Book/Review/dfs_bfs.py
+++ Book/Review/dfs_bfs.py
@@ -392,57 +392,115 @@
 # r1
 # https://www.acmicpc.net/problem/16234
 
+# from collections import deque
+
+# n, l, r = map(int, input().split())
+# arr = []
+# for _ in range(n):
+#     arr.append(list(map(int, input().split())))
+
+# dx = [-1, 0, 1, 0]
+# dy = [0, -1, 0, 1]
+
+# def bfs(x, y, visited):
+#     union = []
+#     union.append((x, y))
+#     sum_u = arr[x][y]
+#     q = deque()
+#     q.append((x, y))
+#     visited[x][y] = True
+
+#     while q:
+#         x, y = q.popleft()
+#         for i in range(4):
+#             nx = x + dx[i]
+#             ny = y + dy[i]
+            
+#             if nx >= 0 and nx < n and ny >= 0 and ny < n:
+#                 if not visited[nx][ny] and l <= abs(arr[x][y] - arr[nx][ny]) and abs(arr[x][y] - arr[nx][ny]) <= r:
+#                     q.append((nx, ny))
+#                     union.append((nx, ny))
+#                     sum_u += arr[nx][ny]
+#                     visited[nx][ny] = True
+#     return union, sum_u
+
+# ans = 0
+
+# while True:
+#     chk = False
+#     visited = [[False] * n for _ in range(n)]
+
+#     for i in range(n):
+#         for j in range(n):
+#             if not visited[i][j]:
+#                 union, sum_u = bfs(i, j, visited)
+#                 if len(union) > 1:
+#                     chk = True
+#                     sum_u = sum_u // len(union)
+#                     for k in union:
+#                         x, y = k
+#                         arr[x][y] = sum_u
+#     if chk:
+#         ans += 1
+#     if not chk:
+#         break
+
+# print(ans)
+
+
+
+# 블록 이동하기
+# r1 x
+# https://programmers.co.kr/learn/courses/30/lessons/60063
+
 from collections import deque
 
-n, l, r = map(int, input().split())
-arr = []
-for _ in range(n):
-    arr.append(list(map(int, input().split())))
+def get_next_pos(pos, board):
+    next_pos = []
+    pos = list(pos)
+    pos1_x, pos1_y, pos2_x, pos2_y = pos[0][0], pos[0][1], pos[1][0], pos[1][1]
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
+    for i in range(4):
+        pos1_next_x, pos1_next_y, pos2_next_x, pos2_next_y = pos1_x + dx[i], pos1_y + dy[i], pos2_x + dx[i], pos2_y + dy[i]
 
-dx = [-1, 0, 1, 0]
-dy = [0, -1, 0, 1]
+        if board[pos1_next_x][pos1_next_y] == 0 and board[pos2_next_x][pos2_next_y] == 0:
+            next_pos.append({(pos1_next_x, pos1_next_y), (pos2_next_x, pos2_next_y)})
+    
+    if pos1_x == pos2_x:
+        for i in [-1, 1]:
+            if board[pos1_x + i][pos1_y] == 0 and board[pos2_x + i][pos2_y] == 0:
+                next_pos.append({(pos1_x, pos1_y), (pos2_x + i, pos2_y)})
+                next_pos.append({(pos2_x, pos2_y), (pos2_x + i, pos2_y)})
+    elif pos1_y == pos2_y:
+        for i in [-1, 1]:
+            if board[pos1_x][pos1_y + i] == 0 and board[pos2_x][pos2_y + i] == 0:
+                next_pos.append({(pos1_x, pos1_y), (pos2_x, pos2_y + i)})
+                next_pos.append({(pos2_x, pos2_y), (pos2_x, pos2_y + i)})
+    
+    return next_pos
 
-def bfs(x, y, visited):
-    union = []
-    union.append((x, y))
-    sum_u = arr[x][y]
-    q = deque()
-    q.append((x, y))
-    visited[x][y] = True
-
-    while q:
-        x, y = q.popleft()
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            
-            if nx >= 0 and nx < n and ny >= 0 and ny < n:
-                if not visited[nx][ny] and l <= abs(arr[x][y] - arr[nx][ny]) and abs(arr[x][y] - arr[nx][ny]) <= r:
-                    q.append((nx, ny))
-                    union.append((nx, ny))
-                    sum_u += arr[nx][ny]
-                    visited[nx][ny] = True
-    return union, sum_u
-
-ans = 0
-
-while True:
-    chk = False
-    visited = [[False] * n for _ in range(n)]
-
+def solution(board):
+    n = len(board)
+    new_board = [[1] * (n + 2) for _ in range(n + 2)]
     for i in range(n):
         for j in range(n):
-            if not visited[i][j]:
-                union, sum_u = bfs(i, j, visited)
-                if len(union) > 1:
-                    chk = True
-                    sum_u = sum_u // len(union)
-                    for k in union:
-                        x, y = k
-                        arr[x][y] = sum_u
-    if chk:
-        ans += 1
-    if not chk:
-        break
+            new_board[i + 1][j + 1] = board[i][j]
+    
+    q = deque()
+    visited = []
+    pos = {(1, 1), (1, 2)}
+    q.append((pos, 0))
+    visited.append(pos)
 
-print(ans)
+    while q:
+        pos, cost = q.popleft()
+        if (n, n) in pos:
+            return cost
+        for next_pos in get_next_pos(pos, new_board):
+            if next_pos not in visited:
+                q.append((next_pos, cost + 1))
+                visited.append(next_pos)
+    return 0
+
+print(solution([[0, 0, 0, 1, 1],[0, 0, 0, 1, 0],[0, 1, 0, 1, 1],[1, 1, 0, 0, 1],[0, 0, 0, 0, 0]]))
