23f473ea449b2941bac0a7f05983819f5f47870f
sjjam <shimjm21@gmail.com>
2021-02-18 16:33:01 +0900

    02/18 sorting 1~3 review

---
+++ Book/Review/sorting.py
@@ -0,0 +1,46 @@
+# 6-1
+# r1
+
+# n = int(input())
+# data = []
+# for i in range(n):
+#     data.append(int(input()))
+
+# data.sort(reverse=True)
+
+# for i in data:
+#     print(i, end=' ')
+
+
+
+# 6-2
+# r1
+
+# n = int(input())
+# data = []
+# for i in range(n):
+#     name, age = input().split()
+#     data.append((name, int(age)))
+
+# data.sort(key=lambda x:x[1])
+
+# for i in data:
+#     print(i[0], end=' ')
+
+
+
+# 6-3
+# r1
+
+n, k = map(int, input().split())
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+
+a.sort()
+b.sort(reverse=True)
+
+for i in range(k):
+    if a[i] < b[i]:
+        a[i], b[i] = b[i], a[i]
+
+print(sum(a))
