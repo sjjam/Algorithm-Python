19fea87302f14b26402836b082eedda580e2dc46
sjjam <shimjm21@gmail.com>
2021-02-19 21:49:19 +0900

    02/19 sorting Q23~24 review

--- Book/Review/sorting.py
+++ Book/Review/sorting.py
@@ -32,15 +32,46 @@
 # 6-3
 # r1
 
-n, k = map(int, input().split())
-a = list(map(int, input().split()))
-b = list(map(int, input().split()))
+# n, k = map(int, input().split())
+# a = list(map(int, input().split()))
+# b = list(map(int, input().split()))
 
-a.sort()
-b.sort(reverse=True)
+# a.sort()
+# b.sort(reverse=True)
 
-for i in range(k):
-    if a[i] < b[i]:
-        a[i], b[i] = b[i], a[i]
+# for i in range(k):
+#     if a[i] < b[i]:
+#         a[i], b[i] = b[i], a[i]
 
-print(sum(a))
+# print(sum(a))
+
+
+
+# 국영수
+# r1
+# https://www.acmicpc.net/problem/10825
+
+# n = int(input())
+# data = []
+# for i in range(n):
+#     name, s1, s2, s3 = input().split()
+#     data.append([name, int(s1), int(s2), int(s3)])
+
+# data.sort(key=lambda x:(-x[1], x[2], -x[3], x[0]))
+
+# for i in data:
+#     print(i[0])
+
+
+
+# 안테나
+# r1
+# https://www.acmicpc.net/problem/18310
+
+n = int(input())
+pos = list(map(int, input().split()))
+pos.sort()
+
+ans = pos[(n - 1) // 2]
+
+print(ans)
