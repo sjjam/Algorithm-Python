03c6e273a5d950e1535f59410b710cc4c12550bf
sjjam <shimjm21@gmail.com>
2021-03-04 15:27:19 +0900

    03/04 입국심사

--- programmers/type/이분탐색/43238.py
+++ programmers/type/이분탐색/43238.py
@@ -1,8 +1,24 @@
 # 입국심사
 # https://programmers.co.kr/learn/courses/30/lessons/43238
+# r1 x
 
 def solution(n, times):
     answer = 0
-    return answer
+    start, end = 1, max(times) * n
 
-solution(6, [7, 10])
+    while start <= end:
+        mid = (start + end) // 2
+        cnt = 0
+        
+        for i in times:
+            cnt += mid // i
+            if cnt >= n:
+                break
+        
+        if cnt >= n:
+            answer = mid
+            end = mid - 1
+        else:
+            start = mid + 1
+    
+    return answer
