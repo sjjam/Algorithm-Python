023bbc4d29d47fa68b6b0494a6eb7b0a49b02cea
sjjam <shimjm21@gmail.com>
2021-03-29 21:49:25 +0900

    03/29 graph Q41 review

--- Book/Review/graph.py
+++ Book/Review/graph.py
@@ -79,39 +79,78 @@
 # 커리큘럼
 # r1 x
 
-from collections import deque
-import copy
+# from collections import deque
+# import copy
 
-n = int(input())
-indegree = [0] * (n + 1)
-time = [0] * (n + 1)
-graph = [[] for _ in range(n + 1)]
+# n = int(input())
+# indegree = [0] * (n + 1)
+# time = [0] * (n + 1)
+# graph = [[] for _ in range(n + 1)]
+
+# for i in range(1, n + 1):
+#     data = list(map(int, input().split()))
+#     time[i] = data[0]
+#     for x in data[1:-1]:
+#         indegree[i] += 1
+#         graph[x].append(i)
+
+# def topology_sort():
+#     result = copy.deepcopy(time)
+#     q = deque()
+
+#     for i in range(1, n + 1):
+#         if indegree[i] == 0:
+#             q.append(i)
+    
+#     while q:
+#         now = q.popleft()
+#         for i in graph[now]:
+#             result[i] = max(result[i], result[now] + time[i])
+#             indegree[i] -= 1
+
+#             if indegree[i] == 0:
+#                 q.append(i)
+    
+#     for i in range(1, n + 1):
+#         print(result[i])
+
+# topology_sort()
+
+
+
+# 여행 계획
+# r1
+
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+n, m = map(int, input().split())
+parent = [0] * (n + 1)
+ans = 'YES'
 
 for i in range(1, n + 1):
+    parent[i] = i
+
+for i in range(n):
     data = list(map(int, input().split()))
-    time[i] = data[0]
-    for x in data[1:-1]:
-        indegree[i] += 1
-        graph[x].append(i)
+    for j in range(n):
+        if data[j] == 1:
+            union_parent(parent, i + 1, j + 1)
 
-def topology_sort():
-    result = copy.deepcopy(time)
-    q = deque()
+plan = list(map(int, input().split()))
 
-    for i in range(1, n + 1):
-        if indegree[i] == 0:
-            q.append(i)
-    
-    while q:
-        now = q.popleft()
-        for i in graph[now]:
-            result[i] = max(result[i], result[now] + time[i])
-            indegree[i] -= 1
+for i in range(len(plan) - 1):
+    if parent[plan[i]] != parent[plan[i + 1]]:
+        ans = 'NO'
 
-            if indegree[i] == 0:
-                q.append(i)
-    
-    for i in range(1, n + 1):
-        print(result[i])
-
-topology_sort()
+print(ans)
