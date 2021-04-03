1fc55c2731e778ba0261bda769cf6cf23a457bd5
sjjam <shimjm21@gmail.com>
2021-04-03 20:10:05 +0900

    04/03 graph Q44 review

--- Book/Review/graph.py
+++ Book/Review/graph.py
@@ -195,6 +195,48 @@
 # 어두운 길
 # r1
 
+# def find_parent(parent, x):
+#     if parent[x] != x:
+#         parent[x] = find_parent(parent, parent[x])
+#     return parent[x]
+
+# def union_parent(parent, a, b):
+#     a = find_parent(parent, a)
+#     b = find_parent(parent, b)
+#     if a < b:
+#         parent[b] = a
+#     else:
+#         parent[a] = b
+
+# n, m = map(int, input().split())
+# parent = [0] * n
+# edges = []
+# result = 0
+
+# for i in range(n):
+#     parent[i] = i
+
+# for i in range(m):
+#     x, y, z = map(int, input().split())
+#     result += z
+#     edges.append((z, x, y))
+
+# edges.sort()
+
+# for edge in edges:
+#     cost, x, y = edge
+#     if find_parent(parent, x) != find_parent(parent, y):
+#         union_parent(parent, x, y)
+#         result -= cost
+
+# print(result)
+
+
+
+# 행성 터널
+# r1 x
+# https://www.acmicpc.net/problem/2887
+
 def find_parent(parent, x):
     if parent[x] != x:
         parent[x] = find_parent(parent, parent[x])
@@ -208,25 +250,39 @@ def union_parent(parent, a, b):
     else:
         parent[a] = b
 
-n, m = map(int, input().split())
-parent = [0] * n
+n = int(input())
+parent = [0] * (n + 1)
 edges = []
 result = 0
 
-for i in range(n):
+for i in range(1, n + 1):
     parent[i] = i
 
-for i in range(m):
-    x, y, z = map(int, input().split())
-    result += z
-    edges.append((z, x, y))
+x = []
+y = []
+z = []
+
+for i in range(1, n + 1):
+    data = list(map(int, input().split()))
+    x.append((data[0], i))
+    y.append((data[1], i))
+    z.append((data[2], i))
+
+x.sort()
+y.sort()
+z.sort()
+
+for i in range(n - 1):
+    edges.append((x[i + 1][0] - x[i][0], x[i][1], x[i + 1][1]))
+    edges.append((y[i + 1][0] - y[i][0], y[i][1], y[i + 1][1]))
+    edges.append((z[i + 1][0] - z[i][0], z[i][1], z[i + 1][1]))
 
 edges.sort()
 
 for edge in edges:
-    cost, x, y = edge
-    if find_parent(parent, x) != find_parent(parent, y):
-        union_parent(parent, x, y)
-        result -= cost
+    cost, a, b = edge
+    if find_parent(parent, a) != find_parent(parent, b):
+        union_parent(parent, a, b)
+        result += cost
 
 print(result)
