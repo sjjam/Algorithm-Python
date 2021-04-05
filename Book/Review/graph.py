508260b029a97c9e2aba87a2c58f6d456904b8bc
sjjam <shimjm21@gmail.com>
2021-04-05 21:53:08 +0900

    04/05 graph Q45 review

--- Book/Review/graph.py
+++ Book/Review/graph.py
@@ -237,52 +237,123 @@
 # r1 x
 # https://www.acmicpc.net/problem/2887
 
-def find_parent(parent, x):
-    if parent[x] != x:
-        parent[x] = find_parent(parent, parent[x])
-    return parent[x]
+# def find_parent(parent, x):
+#     if parent[x] != x:
+#         parent[x] = find_parent(parent, parent[x])
+#     return parent[x]
 
-def union_parent(parent, a, b):
-    a = find_parent(parent, a)
-    b = find_parent(parent, b)
-    if a < b:
-        parent[b] = a
-    else:
-        parent[a] = b
+# def union_parent(parent, a, b):
+#     a = find_parent(parent, a)
+#     b = find_parent(parent, b)
+#     if a < b:
+#         parent[b] = a
+#     else:
+#         parent[a] = b
 
-n = int(input())
-parent = [0] * (n + 1)
-edges = []
-result = 0
+# n = int(input())
+# parent = [0] * (n + 1)
+# edges = []
+# result = 0
 
-for i in range(1, n + 1):
-    parent[i] = i
+# for i in range(1, n + 1):
+#     parent[i] = i
 
-x = []
-y = []
-z = []
+# x = []
+# y = []
+# z = []
 
-for i in range(1, n + 1):
+# for i in range(1, n + 1):
+#     data = list(map(int, input().split()))
+#     x.append((data[0], i))
+#     y.append((data[1], i))
+#     z.append((data[2], i))
+
+# x.sort()
+# y.sort()
+# z.sort()
+
+# for i in range(n - 1):
+#     edges.append((x[i + 1][0] - x[i][0], x[i][1], x[i + 1][1]))
+#     edges.append((y[i + 1][0] - y[i][0], y[i][1], y[i + 1][1]))
+#     edges.append((z[i + 1][0] - z[i][0], z[i][1], z[i + 1][1]))
+
+# edges.sort()
+
+# for edge in edges:
+#     cost, a, b = edge
+#     if find_parent(parent, a) != find_parent(parent, b):
+#         union_parent(parent, a, b)
+#         result += cost
+
+# print(result)
+
+
+
+# 최종 순위
+# r1 x
+# https://www.acmicpc.net/problem/3665
+
+from collections import deque
+
+for tc in range(int(input())):
+    n = int(input())
+    indegree = [0] * (n + 1)
+    graph = [[False] * (n + 1) for _ in range(n + 1)]
+
     data = list(map(int, input().split()))
-    x.append((data[0], i))
-    y.append((data[1], i))
-    z.append((data[2], i))
+    for i in range(n):
+        for j in range(i + 1, n):
+            graph[data[i]][data[j]] = True
+            indegree[data[j]] += 1
 
-x.sort()
-y.sort()
-z.sort()
+    m = int(input())
+    for i in range(m):
+        a, b = map(int, input().split())
+        if graph[a][b]:
+            graph[a][b] = False
+            graph[b][a] = True
+            indegree[a] += 1
+            indegree[b] -= 1
+        else:
+            graph[a][b] = True
+            graph[b][a] = False
+            indegree[a] -= 1
+            indegree[b] += 1
 
-for i in range(n - 1):
-    edges.append((x[i + 1][0] - x[i][0], x[i][1], x[i + 1][1]))
-    edges.append((y[i + 1][0] - y[i][0], y[i][1], y[i + 1][1]))
-    edges.append((z[i + 1][0] - z[i][0], z[i][1], z[i + 1][1]))
+    result = []
+    q = deque()
+    
+    for i in range(1, n + 1):
+        if indegree[i] == 0:
+            q.append(i)
+    
+    certain = True
+    cycle = False
 
-edges.sort()
+    for i in range(n):
+        if len(q) == 0:
+            cycle = True
+            break
 
-for edge in edges:
-    cost, a, b = edge
-    if find_parent(parent, a) != find_parent(parent, b):
-        union_parent(parent, a, b)
-        result += cost
+        if len(q) >= 2:
+            certain = False
+            break
 
-print(result)
+        now = q.popleft()
+        result.append(now)
+
+        for j in range(1, n + 1):
+            if graph[now][j]:
+                indegree[j] -= 1
+
+                if indegree[j] == 0:
+                    q.append(j)
+
+    if cycle:
+        print('IMPOSSIBLE')
+    elif not certain:
+        print('?')
+    else:
+        for i in result:
+            print(i, end=' ')
+        print()
