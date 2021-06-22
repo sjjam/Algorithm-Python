a20d325f620463debad933f325bacd8724bf1984
sjjam <shimjm21@gmail.com>
2021-06-22 22:08:38 +0900

    06/22 골목 대장 호석 - 효율성 2 해결

--- baekjoon/20183.py
+++ baekjoon/20183.py
@@ -57,3 +57,53 @@ if answer == INF:
     print(-1)
 else:
     print(answer)
+
+
+
+# 정답
+
+import heapq
+import sys
+
+input = sys.stdin.readline
+
+n, m, a, b, c = map(int, input().split())
+INF = int(1e14) + 1
+graph = [[] for _ in range(n + 1)]
+
+def dijkstra(mid):
+    q = []
+    distance = [INF] * (n + 1)
+    heapq.heappush(q, (0, a))
+    distance[a] = 0
+    while q:
+        dist, now = heapq.heappop(q)
+        if distance[now] != dist:
+            continue
+        for i in graph[now]:
+            if i[1] > mid:
+                continue
+            cost = dist + i[1]
+            if distance[i[0]] > cost:
+                distance[i[0]] = cost
+                heapq.heappush(q, (cost, i[0]))
+    return distance[b] <= c
+
+costs = set()
+for i in range(m):
+    s, e, d = map(int, input().split())
+    graph[s].append((e, d))
+    graph[e].append((s, d))
+    costs.add(d)
+
+costs = sorted(costs)
+L = 0
+R = len(costs)
+while L < R:
+    mid = (L + R) // 2
+    if dijkstra(costs[mid]):
+        R = mid
+    else:
+        L = mid + 1
+
+print(costs[L] if L < len(costs) else -1)
